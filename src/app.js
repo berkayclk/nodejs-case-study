0542cc5ccf6538eb2185abcf9d3f0d0c6b8efc16
berkaycelik <bcelik@rapsodo.com>
2020-09-19 03:25:29 +0300

    add morgan request logger to express middlewares

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import morgan from 'morgan';
 
 import { ApiResponse } from './api/models';
 import { Record } from './routes';
@@ -9,6 +10,7 @@ import { Record } from './routes';
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use(morgan('dev'));
 
 app.use('/records', Record);
 
