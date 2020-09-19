297760c74c5f8522cf8e88edd9837c10f8d9f659
berkaycelik <bcelik@rapsodo.com>
2020-09-20 02:12:22 +0300

    fix the name of the validationError custom error

--- src/errors/modelValidationError.js
+++ src/errors/modelValidationError.js
@@ -4,7 +4,7 @@ import { ErrorCodes } from '../enums';
 export default class ModelValidationError extends CustomErrorBase {
     constructor(message) {
         super();
-        this.name = 'ValidationError';
+        this.name = 'ModelValidationError';
         this.code = ErrorCodes.VALIDATION;
         this.message =
             message || 'An error has been occurred while validating!';
